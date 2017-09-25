c7a9d9e191ef9b3376aae494f8703e3d518de531
EzawaSyuuto <ezawa.syuuto@gmail.com>
2017-09-25 12:27:54 +0900

    StopGenerate()を追加、生成位置をAndoGeneratorを中心にし、周辺で生成されるようになった。
    StopGenerate()　・・・　呼び出すと生成が止まる

--- Misoten8/Assets/ezawaF/cs/AndoGenerator.cs
+++ Misoten8/Assets/ezawaF/cs/AndoGenerator.cs
@@ -6,30 +6,42 @@ public class AndoGenerator : MonoBehaviour {
 
     //プレハブ（白と赤のCube（ボックス））を変数に代入
     public GameObject AndoPrehub;//白のCube（ボックス）
+    public float WitdhHeight;   // ジェネレータからこの変数の範囲内に生成する
+    public bool stop;
 
     private float time;//ボックス生成時間
 
     void Start()
     {
+        WitdhHeight = 10.0f;
         time = 0.5f;
+        stop = false;
     }
 
     void Update()
     {
         //0.5秒間隔でボックス生成。60秒で120個のボックス生成。
-        time -= Time.deltaTime;
-        if (time <= 0.0)
+        if ( !stop )
         {
-            time = 0.5f;
+            time -= Time.deltaTime;
+            if (time <= 0.0)
+            {
+                time = 0.5f;
 
-            //ボックスの出現座標
-            float x = Random.Range(-2.27f, -11.78f);
-            float y = 0.0f;
-            float z = Random.Range(-2.27f, -11.78f);
+                //ボックスの出現座標
+                float x = Random.Range(transform.position.x - WitdhHeight, transform.position.x + WitdhHeight);
+                float y = transform.position.y;
+                float z = Random.Range(transform.position.z - WitdhHeight, transform.position.z + WitdhHeight);
 
-            //ボックス生成
-            Instantiate(AndoPrehub, new Vector3(x, y, z), Quaternion.identity);
+                //ボックス生成
+                Instantiate(AndoPrehub, new Vector3(x, y, z), Quaternion.identity);
 
+            }
         }
+    }
+
+    public void StopGenerate()
+    {
+        stop = true;
     }
 }
