ee29952a4fc0290eae2b876afdee45463f73f2f5
Ando Hiroki <ashsavior17@gmail.com>
2017-09-25 11:10:52 +0900

    画面遷移追加

---
+++ Misoten8/Assets/Ando/scripts/scenetrans.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class scenetrans : MonoBehaviour {
+    [SerializeField]
+    public Score score;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       if (score.GameScore >= 50)
+        {
+            SceneManager.LoadScene("Sample");
+        }
+        Debug.Log("honyarara");
+    }
+}
