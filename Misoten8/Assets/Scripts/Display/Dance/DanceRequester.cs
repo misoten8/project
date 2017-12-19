0d47b6954bab4eaedb9c674893948ae33a1f59c5
JitsukawaHirotaka <jitu8726@gmail.com>
2017-12-19 18:29:10 +0900

    テクスチャを追加

---
+++ Misoten8/Assets/Scripts/Display/Dance/DanceRequester.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// リクエスト投影UIオブジェクト
+/// </summary>
+public class DanceRequester : UIBase 
+{
+
+}
