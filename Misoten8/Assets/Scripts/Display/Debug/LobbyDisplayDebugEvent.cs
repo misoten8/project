7f65f0be1d721f14f1376e6c4c2aca2a67f13638
JitsukawaHirotaka <jitu8726@gmail.com>
2018-01-05 13:34:54 +0900

    ロビーディスプレイにプレイヤーログイン情報UIを追加

--- Misoten8/Assets/Scripts/Display/Debug/LobbyDisplayDebugEvent.cs
+++ Misoten8/Assets/Scripts/Display/Debug/LobbyDisplayDebugEvent.cs
@@ -16,7 +16,14 @@ public class LobbyDisplayDebugEvent : DisplayDebugEventBase
 		debugEvents
 			.eventList = new List<DisplayDebugger.DebugEvents.Element>()
 			{
-
+				new DisplayDebugger.DebugEvents.Element(e.onPlayer1Online, nameof(e.onPlayer1Online)),
+				new DisplayDebugger.DebugEvents.Element(e.onPlayer2Online, nameof(e.onPlayer2Online)),
+				new DisplayDebugger.DebugEvents.Element(e.onPlayer3Online, nameof(e.onPlayer3Online)),
+				new DisplayDebugger.DebugEvents.Element(e.onNavOnline, nameof(e.onNavOnline)),
+				new DisplayDebugger.DebugEvents.Element(e.onPlayer1Offline, nameof(e.onPlayer1Offline)),
+				new DisplayDebugger.DebugEvents.Element(e.onPlayer2Offline, nameof(e.onPlayer2Offline)),
+				new DisplayDebugger.DebugEvents.Element(e.onPlayer3Offline, nameof(e.onPlayer3Offline)),
+				new DisplayDebugger.DebugEvents.Element(e.onNavOffline, nameof(e.onNavOffline)),
 			};
 	}
 }
--- Misoten8/Assets/Scripts/Display/Events/LobbyEvents.cs
+++ Misoten8/Assets/Scripts/Display/Events/LobbyEvents.cs
@@ -1,7 +1,40 @@
-﻿/// <summary>
+﻿using System;
+
+/// <summary>
 /// ロビーディスプレイのUIオブジェクト呼び出しイベントクラス
 /// </summary>
 public class LobbyEvents : EventsBase
 {
-
+	/// <summary>
+	/// プレイヤー1オンライン時実行イベント
+	/// </summary>
+	public Action onPlayer1Online;
+	/// <summary>
+	/// プレイヤー2オンライン時実行イベント
+	/// </summary>
+	public Action onPlayer2Online;
+	/// <summary>
+	/// プレイヤー3オンライン時実行イベント
+	/// </summary>
+	public Action onPlayer3Online;
+	/// <summary>
+	/// 実況ナビオンライン時実行イベント
+	/// </summary>
+	public Action onNavOnline;
+	/// <summary>
+	/// プレイヤー1オフライン時実行イベント
+	/// </summary>
+	public Action onPlayer1Offline;
+	/// <summary>
+	/// プレイヤー2オフライン時実行イベント
+	/// </summary>
+	public Action onPlayer2Offline;
+	/// <summary>
+	/// プレイヤー3オフライン時実行イベント
+	/// </summary>
+	public Action onPlayer3Offline;
+	/// <summary>
+	/// 実況ナビオフライン時実行イベント
+	/// </summary>
+	public Action onNavOffline;
 }
