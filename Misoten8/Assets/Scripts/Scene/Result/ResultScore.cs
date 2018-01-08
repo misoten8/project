2362a672865e880fc6caa2fbc8ee4734dac054f8
JitsukawaHirotaka <jitu8726@gmail.com>
2018-01-08 19:39:31 +0900

    不要なスクリプトを消去

--- Misoten8/Assets/Scripts/Scene/Result/ResultScore.cs
+++
@@ -1,8 +0,0 @@
-﻿/// <summary>
-/// ResultScore クラス
-/// 製作者：実川
-/// </summary>
-public static class ResultScore 
-{
-	public static int[] scoreArray = new int[Define.METER_NUM_MAX] { 1, 2, 3, 4, 5 };
-}
