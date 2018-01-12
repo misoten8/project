a2a0ae7972720d89382d9be00b7bb5faee2c009e
JitsukawaHirotaka <jitu8726@gmail.com>
2018-01-13 07:12:33 +0900

    実況画面を実装

---
+++ Misoten8/Assets/Scripts/Display/Nav/NavAnnounceInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TextFx;
+using UnityEngine;
+using Misoten8Utility;
+
+/// <summary>
+/// NavAnnounceInfo クラス
+/// </summary>
+public class NavAnnounceInfo : MonoBehaviour 
+{
+	[SerializeField]
+	List<string> _messages = new List<string>();
+
+	[SerializeField]
+	private TextFxTextMeshProUGUI _message;
+
+	public uint readIndex = 0;
+
+	public bool isRun = true;
+
+	public void Start()
+	{
+		StartCoroutine(StepDo());
+	}
+
+	private IEnumerator StepDo()
+	{
+		do
+		{
+			// メッセージの設定
+			_message.SetText(_messages[(int)readIndex]);
+
+			// メッセージのスクロールアニメーション
+			_message.AnimationManager.PlayAnimation();
+
+			readIndex++;
+			if(_messages.Count >= readIndex)
+			{
+				readIndex = 0;
+			}
+			yield return new WaitForSeconds(10.0f);
+		} while (isRun);
+	}
+}
